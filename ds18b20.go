48f3ce51eb97a3de019b4d348b715a606e276c01
Marcel da Silva <dasilva.marcel@gmail.com>
2019-12-15 22:59:18 +0100

    Initial commit

---
+++ ds18b20.go
@@ -0,0 +1,73 @@
+package ds18b20
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var busPath = "/sys/bus/w1/devices/"
+var busMasterPath = busPath + "w1_bus_master1/"
+var busMaster = busMasterPath + "w1_master_slaves"
+var sensorFile = "w1_slave"
+
+type Sensor struct {
+	Name string
+}
+
+// GetSensors get known sensors
+func GetSensors() (sensors []*Sensor, err error) {
+	data, err := ioutil.ReadFile(busMaster)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, sensorName := range strings.Split(string(data), "\n") {
+		if sensorName == "" {
+			continue
+		}
+		sensors = append(sensors, &Sensor{Name: sensorName})
+	}
+
+	return sensors, nil
+}
+
+// GetTemperature get the temperature for a sensor
+//
+// the sensor device's content looks like this:
+// 33 00 4b 46 ff ff 02 10 f4 : crc=f4 YES
+// 33 00 4b 46 ff ff 02 10 f4 t=25625
+func (s *Sensor) GetTemperature() (temp float64, err error) {
+	data, err := ioutil.ReadFile(busPath + s.Name + "/w1_slave")
+	if err != nil {
+		return 0.0, fmt.Errorf("failed to read sensor %q: %v", s.Name, err)
+	}
+
+	lines := strings.Split(string(data), "\n")
+	if len(lines) != 3 {
+		return 0.0, fmt.Errorf("can not parse temperature, wrong file format")
+	}
+
+	// 49 01 4b 46 7f ff 07 10 f6 : crc=f6 YES
+	regex := regexp.MustCompile(`(?:[0-9a-f]{2} ){9}: crc=(?:[0-9a-f]{2} )YES`)
+	if !regex.MatchString(lines[0]) {
+		return 0.0, fmt.Errorf("could not verify crc, regex did not match")
+	}
+
+	// 33 00 4b 46 ff ff 02 10 f4 t=25625
+	regex = regexp.MustCompile(`(?:[0-9a-f]{2} ){9}t=(?P<temperature>[0-9]+)`)
+
+	match := regex.FindStringSubmatch(lines[1])
+	if match == nil {
+		return 0.0, fmt.Errorf("could not extract temperature, regex did not match")
+	}
+	temperature, err := strconv.ParseInt(match[1], 10, 64)
+	if err != nil {
+		return 0.0, fmt.Errorf("could not extract temperature, parse to float failed")
+	}
+	temp = float64(temperature) / 1000
+
+	return temp, nil
+}
---
+++ ds18b20_test.go
@@ -0,0 +1,120 @@
+package ds18b20
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testSensors = []string{"28-000000000000", "28-000000000001", "28-000000000002", "28-000000000003", "28-000000000004", "28-000000000005"}
+
+func TestMain(m *testing.M) {
+	busPath = "testdata/"
+	busMasterPath = busPath + "w1_bus_master1/"
+	busMaster = busMasterPath + "w1_master_slaves"
+
+	exitVal := m.Run()
+
+	os.Exit(exitVal)
+}
+
+func TestGetSensors(t *testing.T) {
+	t.Log("Running TestGetSensors")
+
+	testCases := []struct {
+		name            string
+		busMaster       string
+		expectedSensors []*Sensor
+		expectedError   string
+	}{
+		{
+			name: "Correct sensors",
+			expectedSensors: []*Sensor{
+				&Sensor{Name: testSensors[1]},
+				&Sensor{Name: testSensors[2]},
+				&Sensor{Name: testSensors[3]},
+			},
+		},
+		{
+			name:          "wrong bus master",
+			busMaster:     "wrongfile",
+			expectedError: "open wrongfile: no such file or directory",
+		},
+	}
+
+	for _, testCase := range testCases {
+		if testCase.busMaster != "" {
+			busMaster = testCase.busMaster
+		}
+		sensors, err := GetSensors()
+
+		// assert error
+		if testCase.expectedError != "" {
+			assert.EqualError(t, err, testCase.expectedError)
+			continue
+		} else {
+			assert.NoError(t, err)
+		}
+
+		assert.Equal(t, testCase.expectedSensors, sensors)
+	}
+}
+
+func TestGetTemperature(t *testing.T) {
+	t.Log("Running TestGetTemperature")
+	testCases := []struct {
+		name          string
+		sensor        *Sensor
+		expectedTemp  float64
+		expectedError string
+	}{
+		{
+			name:          "Not existing sensor",
+			sensor:        &Sensor{Name: testSensors[0]},
+			expectedError: `failed to read sensor "28-000000000000": open testdata/28-000000000000/w1_slave: no such file or directory`,
+		},
+		{
+			name:         "Sensor with correct CRC",
+			sensor:       &Sensor{Name: testSensors[1]},
+			expectedTemp: 20.562,
+		},
+		{
+			name:          "Sensor with incorrect CRC",
+			sensor:        &Sensor{Name: testSensors[2]},
+			expectedError: "could not verify crc, regex did not match",
+		},
+		{
+			name:          "Sensor with incorrect temperature format",
+			sensor:        &Sensor{Name: testSensors[3]},
+			expectedError: "could not extract temperature, regex did not match",
+		},
+		{
+			name:          "Sensor with no content",
+			sensor:        &Sensor{Name: testSensors[4]},
+			expectedError: "can not parse temperature, wrong file format",
+		},
+		{
+			name:          "Sensor with strange temperature value",
+			sensor:        &Sensor{Name: testSensors[5]},
+			expectedError: "could not extract temperature, parse to float failed",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Logf("- test %q", testCase.name)
+		got, err := testCase.sensor.GetTemperature()
+		t.Logf("  - error got: %v, error expected: %v\n", err, testCase.expectedError)
+
+		// assert error
+		if testCase.expectedError != "" {
+			assert.EqualError(t, err, testCase.expectedError)
+			continue
+		} else {
+			assert.NoError(t, err)
+		}
+
+		// assert value is correct
+		assert.Equal(t, testCase.expectedTemp, got, "Received the expected temperature")
+	}
+}
